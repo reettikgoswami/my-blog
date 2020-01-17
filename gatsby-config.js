b274a7c5ae5f660661d4c7a031e0826249b39b28
reettik <reettikgoswami1997@gmail.com>
2020-01-17 13:49:45 +0530

    my blog

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `my blog`,
+    author: `Reettik Goswami`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/reettik`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/reettik97`,
+      },
+    ],
+  },
+}
